190b20b7ff0f8880ebd3e616bfa1cfce3671fc86
Ashot Israelyan <ashot@picsart.com>
2023-07-11 15:03:43 +0400

    Client project init

---
+++ README.md
@@ -0,0 +1,3 @@
+# GraphQL Chat
+
+This project shows how to use subscriptions with Apollo Server for Express, Apollo Client, and GraphQL-WS.
---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import 'bulma/css/bulma.css';
+import './style.css';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
